fef224bf79bc5afb3577e7d46af44ef5ffb2c7d2
Neilansh Rajpal <neilanshjr@gmail.com>
2023-08-22 21:10:36 +0530

    added code to edit BUDGET field

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,11 +3,39 @@ import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
     // get state variables from the Context
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch } = useContext(AppContext);
+
+    // handlers for incrementing and decrementing budget
+    const increaseBudget = (event) => {
+        if(event.current.value > 20000){
+            alert('Budget cannot exceed 20000');
+            return;
+        }
+
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: budget + 10,
+        });
+    };
+
+    const decreaseBudget = () => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: budget - 10,
+        });
+    };
+
     return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
+        <div className='alert alert-secondary input-group-prepend'>
+            <label className="input-group-text" htmlFor="inputGroupSelect01">Budget: </label>
+            <input type="number" value={budget} class="form-control input-number" min="1" max="20000"  />
+            <button type="button" class="btn btn-default btn-number" data-type="minus" data-field="quant[1]" onClick={increaseBudget}>
+                <span class="glyphicon glyphicon-minus"></span>
+            </button>
+            <button type="button" class="btn btn-default btn-number" data-type="plus" data-field="quant[1]" onClick={decreaseBudget}>
+                <span class="glyphicon glyphicon-plus"></span>
+            </button>
+            </div>
     );
 };
 export default Budget;

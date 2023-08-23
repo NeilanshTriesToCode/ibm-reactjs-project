3a4d90a66e7c3f45b1528a6bd34b9421a37c0b54
Neilansh Rajpal <neilanshjr@gmail.com>
2023-08-23 20:33:03 +0530

    completed task 2

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,39 +3,35 @@ import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
     // get state variables from the Context
-    const { budget, dispatch } = useContext(AppContext);
+    const { budget, currency, dispatch } = useContext(AppContext);
 
-    // handlers for incrementing and decrementing budget
-    const increaseBudget = (event) => {
-        if(event.current.value > 20000){
-            alert('Budget cannot exceed 20000');
+    // handler for incrementing or decrementing budget
+    const handleBudget = (value) => {
+        if(value > 20000){
+            alert(`Budget cannot exceed ${currency}20000`);
             return;
         }
 
         dispatch({
             type: 'SET_BUDGET',
-            payload: budget + 10,
-        });
-    };
-
-    const decreaseBudget = () => {
-        dispatch({
-            type: 'SET_BUDGET',
-            payload: budget - 10,
+            payload: value,
         });
     };
 
     return (
-        <div className='alert alert-secondary input-group-prepend'>
-            <label className="input-group-text" htmlFor="inputGroupSelect01">Budget: </label>
-            <input type="number" value={budget} class="form-control input-number" min="1" max="20000"  />
-            <button type="button" class="btn btn-default btn-number" data-type="minus" data-field="quant[1]" onClick={increaseBudget}>
-                <span class="glyphicon glyphicon-minus"></span>
-            </button>
-            <button type="button" class="btn btn-default btn-number" data-type="plus" data-field="quant[1]" onClick={decreaseBudget}>
-                <span class="glyphicon glyphicon-plus"></span>
-            </button>
+        <div class="input-group mb-3">
+            <div class="input-group-text">
+                <label >Budget: £</label>
+                <input
+                className='form-control m-1'
+                type='number'
+                id='cost'
+                value={budget}
+                step={10}
+                onChange={(event) => handleBudget(event.target.value)}>
+                </input>
             </div>
+    </div>
     );
 };
 export default Budget;
